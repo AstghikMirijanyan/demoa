dd78be94ef40335966f1bfca8e5087bf20a5e42f
Mirijanyan <astghik.mirijanyan@gmail.com>
2021-02-10 08:18:18 +0400

    initial commit

---
+++ database/migrations/2021_02_08_211439_create_friends_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFriendsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friends', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->bigInteger('sender_id')->length(20)->unsigned();
+            $table->bigInteger('receiver_id')->length(20)->unsigned();
+            $table->enum('status', array('approved','pending','rejected'))->nullable();
+
+        });
+
+        Schema::table('friends', function($table) {
+            $table->foreign('sender_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('receiver_id')->references('id')->on('users')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('friends');
+    }
+}
